6204d26613c72d19224231beb8af915171292786
olight0 <157029034+olight0@users.noreply.github.com>
2025-05-04 18:19:16 +0300

    Add files via upload

---
+++ floorp-browser-void.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+echo "Please run the script in your home dir"
+echo "Please Enter the full path of your home dir (/home/<YOURNAME>/)"
+read homedir
+xpbs-install -Syu wget
+wget https://github.com/Floorp-Projects/Floorp/releases/download/v11.26.0/floorp-11.26.0.linux-x86_64.tar.bz2
+tar -xvjf floorp-11.26.0.linux-x86_64.tar.bz2
+cd floorp
+sudo ln -s $homedir/floorp/floorp-bin /bin/floorp
