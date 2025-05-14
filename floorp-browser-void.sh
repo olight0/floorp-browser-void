7644cc34912a7ed4d4347e14df1323eb6db4d103
olight0 <157029034+olight0@users.noreply.github.com>
2025-05-14 15:37:10 +0300

    Update floorp-browser-void.sh

--- floorp-browser-void.sh
+++ floorp-browser-void.sh
@@ -3,7 +3,7 @@ echo "Please run the script in your home dir"
 echo "Please Enter the full path of your home dir (/home/<YOURNAME>/)"
 read homedir
 xpbs-install -Syu wget
-wget https://github.com/Floorp-Projects/Floorp/releases/download/v11.26.0/floorp-11.26.0.linux-x86_64.tar.bz2
+wget https://github.com/Floorp-Projects/Floorp/releases/download/v11.26.1/floorp-11.26.1.linux-x86_64.tar.bz2
 tar -xvjf floorp-11.26.0.linux-x86_64.tar.bz2
 cd floorp
 sudo ln -s $homedir/floorp/floorp-bin /bin/floorp
